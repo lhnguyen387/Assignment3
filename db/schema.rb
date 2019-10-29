b1a96310f198f36a5643270c7df7a20de5e23424
unknown <lhnguyen38@uh.edu>
2019-10-28 21:37:08 -0500

    got appointment table set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_012411) do
+ActiveRecord::Schema.define(version: 2019_10_29_023003) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "visit"
+    t.integer "physician_id", null: false
+    t.integer "patient_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+    t.index ["physician_id"], name: "index_appointments_on_physician_id"
+  end
 
   create_table "offices", force: :cascade do |t|
     t.string "street_address"
@@ -22,6 +32,12 @@ ActiveRecord::Schema.define(version: 2019_10_29_012411) do
     t.index ["physician_id"], name: "index_offices_on_physician_id"
   end
 
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "physicians", force: :cascade do |t|
     t.string "name"
     t.string "speciality"
@@ -29,5 +45,7 @@ ActiveRecord::Schema.define(version: 2019_10_29_012411) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "appointments", "patients"
+  add_foreign_key "appointments", "physicians"
   add_foreign_key "offices", "physicians"
 end
